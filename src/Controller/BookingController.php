b3eb971a3dd86647b687eff689616e82c2bf1ffb
Sathish <meet.sathish@gmail.com>
2019-11-15 14:06:34 +0400

    Booking rest controller and form

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use App\Entity\Booking;
+use App\Form\BookingType;
+/**
+ * Booking controller.
+ * @Route("/api", name="api_")
+ */
+class BookingController extends FOSRestController
+{
+  /**
+   * Lists all Booking.
+   * @Rest\Get("/booking")
+   *
+   * @return Response
+   */
+  public function getBookingAction()
+  {
+    $repository = $this->getDoctrine()->getRepository(Booking::class);
+    $booking = $repository->findall();
+    return $this->handleView($this->view($booking));
+  }
+  /**
+   * Create Booking.
+   * @Rest\Post("/movie")
+   *
+   * @return Response
+   */
+  public function postBookingAction(Request $request)
+  {
+    $booking = new Booking();
+    $form = $this->createForm(BookingType::class, $booking);
+    $data = json_decode($request->getContent(), true);
+    $form->submit($data);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($booking);
+      $em->flush();
+      return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+    }
+    return $this->handleView($this->view($form->getErrors()));
+  }
+}
