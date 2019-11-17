b82ff087fb1a50105314d9240a3a22c0af9a75b9
Sathish <meet.sathish@gmail.com>
2019-11-17 20:21:06 +0400

    Bookign repo assertion

--- src/Tests/BookingRepositoryTest.php
+++ src/Tests/BookingRepositoryTest.php
@@ -26,10 +26,57 @@ class BookingRepositoryTest extends KernelTestCase
     {
         $booking = $this->entityManager
             ->getRepository(Booking::class)
-            ->findOneBy(['hotelname' => 'Priceless widget'])
-        ;
+            ->findOneBy(['hotelname' => 'Priceless widget']);
 
-        $this->assertSame(14.50, $booking->getPrice());
+        $this->assertSame('Priceless widget', $booking->getHotelname());
+    }
+    
+    public function testSearchByPrice()
+    {
+        $booking = $this->entityManager
+            ->getRepository(Booking::class)
+            ->findOneBy(['price' => 14.50]);
+
+        $this->assertSame('14.50', $booking->getPrice());
+    }
+    
+    public function testSearchByAvailability()
+    {
+        $booking = $this->entityManager
+            ->getRepository(Booking::class)
+            ->findOneBy(['availability' => 56]);
+
+        $this->assertSame(56, $booking->getAvailability());
+    }
+    
+    
+    public function testSearchByReputationBadge()
+    {
+        $booking = $this->entityManager
+            ->getRepository(Booking::class)
+            ->findOneBy(['reputationBadge' => 'Yellow']);
+
+        $this->assertSame('Yellow', $booking->getReputationBadge());
+    }
+    
+    
+    public function testSearchByCategory()
+    {
+        $booking = $this->entityManager
+            ->getRepository(Booking::class)
+            ->findOneBy(['category' => 'Hotel']);
+
+        $this->assertSame('Hotel', $booking->getCategory());
+    }
+    
+    
+    public function testSearchByRating()
+    {
+        $booking = $this->entityManager
+            ->getRepository(Booking::class)
+            ->findOneBy(['rating' => 1]);
+
+        $this->assertSame(1, $booking->getRating());
     }
 
     protected function tearDown()
